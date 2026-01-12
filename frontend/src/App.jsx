4073a797b01f78dceaf8f95be06f617f6d29c257
Soujanya02V <soujanyabailawad@gmail.com>
2026-01-12 20:52:53 +0530

    fetch in backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
+  const[name,setName] = useState("");
+  const [message,setMessage] = useState();
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  const handleSubmit = async(e) =>{
+    e.preventDefault();
+    const data = {
+      name,
+      message,
+    };
+
+    const response = await fetch("http://localhost:3000/contact",{
+      method : "POST",
+      headers:
+      {
+        "Content-Type" :"application/json",
+      },
+      body: JSON.stringify(data)
+
+
+    });
+
+    const result = await response.json();
+    console.log(result);
+
+  };
+
+  return(
+        <div style={{ padding: "20px" }}>
+      <h1>React Contact Form</h1>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Name</label>
+          <br />
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+
+        <br />
+
+        <div>
+          <label>Message</label>
+          <br />
+          <textarea
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </div>
+
+        <br />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   )
 }
 
-export default App
+export default App;
