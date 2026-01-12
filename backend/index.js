af417db1b0e4e70290f284f36a10996e859bc8a4
Soujanya02V <soujanyabailawad@gmail.com>
2026-01-12 20:27:14 +0530

    backend added

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.post("/contact",(req,res) => {
+    console.log("form data recieved");
+    console.log(req.body);
+
+    res.json({message: "data recieved successfully"});
+});
+
+const PORT = 3000;
+app.listen(PORT, () =>{
+    console.log(`server running on port ${PORT}`);
+});
+
