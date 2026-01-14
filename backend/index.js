8cfc5bdb487ec6d9a39cac1a8b54cb4e6de29b72
Soujanya02V <soujanyabailawad@gmail.com>
2026-01-14 19:33:24 +0530

    admin

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,19 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv"
 import Contact from "./models/Contact.js"
+import bycrypt from "bcryptjs"
 
 dotenv.config();
 const app = express();
 
+
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+const hashed = await bycrypt.hash("admin123",10);
+console.log(hashed)
 
 
 mongoose.connect(process.env.MONGO_URI)
@@ -43,4 +49,6 @@ const PORT = process.env.PORT||3000;
 app.listen(PORT, () =>{
     console.log(`server running on port ${PORT}`);
 });
+
+
 
